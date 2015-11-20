21edac75d529bef7a504ae5dd4865da890ad387c
Dimitrios Kouzis-Loukas <lookfwd@gmail.com>
2015-11-20 04:16:23 +0000

    autostart wannabe

--- scripts/start_pureftp.sh
+++ scripts/start_pureftp.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Pureftp service
 
-exec /usr/sbin/pure-ftpd -l pam -e -p 30000:30009 -u 1000 -8 UTF-8 -O clf:/var/log/pure-ftpd/transfer.log -B
+#exec /usr/sbin/pure-ftpd -l pam -e -p 30000:30009 -u 1000 -8 UTF-8 -O clf:/var/log/pure-ftpd/transfer.log -B
+exec /etc/init.d/pure-ftpd
 
